46576f56b310ec9bbceed92ad6f09f04c4dc6e40
Rohit Sharma <rohit231186@gmail.com>
2020-09-28 17:33:34 +0530

    added .doc support

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # word-doc-processing
-process a .docx file using textract
+process a .docx or .doc file using textract and doc2txt
 
-install textract using 'pip install textract' and you're ready to go.
+Prerequisites:
+Install textract and doc2txt from you Pycharm IDE
+OR
+install textract using 'pip install textract'.
+install doc2txt using 'pip install docx2txt.
+
+And you're ready to go.
+
+Output:
+creates a read_docx_current_time.txt file for the docx text under the results folder
+creates a read_doc_current_time.txt file for the docx text under the results folder
--- doc-to-text.py
+++ doc-to-text.py
@@ -1,8 +1,16 @@
 import textract
 import time
-text = textract.process("data/cv.docx")
-string1 = str(text.decode("utf-8"))
+import docx2txt
 
+#read a docx file
+text = textract.process("data/cv.docx")
+string_decoded = str(text.decode("utf-8"))
 f = open('./results/'+time.strftime("read_docx_%Y_%m_%d-%H_%M_%S")+'.txt', 'w', newline='', encoding="utf-8")
-f.write(string1)
+f.write(string_decoded)
+f.close()
+
+#read a .doc file
+my_text = docx2txt.process("data/cv.doc")
+f = open('./results/'+time.strftime("read_doc_%Y_%m_%d-%H_%M_%S")+'.txt', 'w', newline='', encoding="utf-8")
+f.write(string_decoded)
 f.close()
